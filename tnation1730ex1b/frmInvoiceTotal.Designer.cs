77b13023912cc5ee04ad9ffbcd660542a2e670fc
Tyler Nation <wn4926xl@go.minnstate.edu>
2019-08-29 18:07:02 -0500

    Completed Ex1b

--- tnation1730ex1b/frmInvoiceTotal.Designer.cs
+++ tnation1730ex1b/frmInvoiceTotal.Designer.cs
@@ -71,10 +71,9 @@
             // 
             this.txtDiscountPercent.Location = new System.Drawing.Point(139, 57);
             this.txtDiscountPercent.Name = "txtDiscountPercent";
-            this.txtDiscountPercent.ReadOnly = true;
             this.txtDiscountPercent.Size = new System.Drawing.Size(100, 20);
             this.txtDiscountPercent.TabIndex = 3;
-            this.txtDiscountPercent.TabStop = false;
+            this.txtDiscountPercent.Text = "5";
             // 
             // label3
             // 
@@ -82,7 +81,7 @@
             this.label3.Location = new System.Drawing.Point(44, 86);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(90, 13);
-            this.label3.TabIndex = 4;
+            this.label3.TabIndex = 6;
             this.label3.Text = "Discount amount:";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
@@ -92,7 +91,7 @@
             this.txtDiscountAmount.Name = "txtDiscountAmount";
             this.txtDiscountAmount.ReadOnly = true;
             this.txtDiscountAmount.Size = new System.Drawing.Size(100, 20);
-            this.txtDiscountAmount.TabIndex = 5;
+            this.txtDiscountAmount.TabIndex = 7;
             this.txtDiscountAmount.TabStop = false;
             // 
             // label4
@@ -101,7 +100,7 @@
             this.label4.Location = new System.Drawing.Point(44, 113);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(34, 13);
-            this.label4.TabIndex = 6;
+            this.label4.TabIndex = 8;
             this.label4.Text = "Total:";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
@@ -111,7 +110,7 @@
             this.txtTotal.Name = "txtTotal";
             this.txtTotal.ReadOnly = true;
             this.txtTotal.Size = new System.Drawing.Size(100, 20);
-            this.txtTotal.TabIndex = 7;
+            this.txtTotal.TabIndex = 9;
             this.txtTotal.TabStop = false;
             // 
             // btnCalculate
@@ -119,7 +118,7 @@
             this.btnCalculate.Location = new System.Drawing.Point(73, 136);
             this.btnCalculate.Name = "btnCalculate";
             this.btnCalculate.Size = new System.Drawing.Size(75, 23);
-            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.TabIndex = 4;
             this.btnCalculate.Text = "&Calculate";
             this.btnCalculate.UseVisualStyleBackColor = true;
             this.btnCalculate.Click += new System.EventHandler(this.BtnCalculate_Click);
@@ -129,7 +128,7 @@
             this.btnExit.Location = new System.Drawing.Point(164, 136);
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(75, 23);
-            this.btnExit.TabIndex = 3;
+            this.btnExit.TabIndex = 5;
             this.btnExit.Text = "E&xit";
             this.btnExit.UseVisualStyleBackColor = true;
             this.btnExit.Click += new System.EventHandler(this.BtnExit_Click);
--- tnation1730ex1b/frmInvoiceTotal.cs
+++ tnation1730ex1b/frmInvoiceTotal.cs
@@ -21,6 +21,12 @@ namespace tnation1730ex1b
         {
             //txtTotal.Text = "10";
             //txtTotal.ReadOnly = false;
+            txtDiscountAmount.Text = 
+                (Convert.ToDecimal(txtSubtotal.Text) 
+                * Convert.ToDecimal(txtDiscountPercent.Text) / 100).ToString("0.00");
+            txtTotal.Text =
+                (Convert.ToDecimal(txtSubtotal.Text)
+                - Convert.ToDecimal(txtDiscountAmount.Text)).ToString("0.00");
         }
 
         private void BtnExit_Click(object sender, EventArgs e)
