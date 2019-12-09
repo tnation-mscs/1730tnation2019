f7346c8f2594f8788cbf80c457f8e557c31043ed
Tyler Nation <wn4926xl@go.minnstate.edu>
2019-12-09 14:47:41 -0600

    Completed Ex3B

---
+++ tnation1730ex3b1/Ex3bCalculations.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tnation1730ex3b1
+{
+    public class Ex3bCalculations
+    {
+        public static decimal GetDiscountPercent(decimal subtotal)
+        {
+            decimal discountPercent = 0m;
+
+            if (subtotal >= 500m)
+                discountPercent = 0.2m;
+            else
+                discountPercent = 0.1m;
+
+            return discountPercent;
+        }
+
+        public static void GetDiscountPercent(decimal subtotal, out decimal discountPercent)
+        {
+            if (subtotal >= 500m)
+                discountPercent = 0.2m;
+            else
+                discountPercent = 0.1m;
+        }
+
+        public static decimal CalculateFutureValue(decimal monthlyInvestment, decimal monthlyInterestRate, int months)
+        {
+            decimal futureValue = 0m;
+
+            for (int i = 1; i <= months; i++)
+            {
+                futureValue = (futureValue + monthlyInvestment) * (1 + monthlyInterestRate);
+            }
+
+            return futureValue;
+        }
+
+        public static void CalculateFutureValue(decimal monthlyInvestment, decimal monthlyInterestRate, int months, ref decimal futureValue)
+        {
+            for (int i = 1; i <= months; i++)
+            {
+                futureValue = (futureValue + monthlyInvestment) * (1 + monthlyInterestRate);
+            }
+        }
+
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            double celsius = 0.0;
+
+            celsius = (fahrenheit - 32) * 5 / 9;
+
+            return celsius;
+        }
+
+        public static double CelsiusToFahrenheit(double celsius, out double fahrenheit)
+        {
+            fahrenheit = celsius * 1.8 + 32;
+            return fahrenheit;
+        }
+
+        public static decimal GrossPay(decimal hours, decimal payRate)
+        {
+            decimal grossPay = 0.0m;
+
+            if (hours <= 40m)
+            {
+                grossPay = hours * payRate;
+            }
+            else
+                grossPay = (40 * payRate) + (hours - 40) * (payRate * 1.5m);
+
+            return grossPay;
+        }
+
+        public static decimal TotalHours(string strNumbers)
+        {
+            decimal total = 0;
+            int startIndex = 0;
+
+            while (startIndex < strNumbers.LastIndexOf(' '))
+            {
+                int endIndex = strNumbers.IndexOf(' ', startIndex);
+                string strNumber = strNumbers.Substring(startIndex, endIndex - startIndex);
+                Decimal number = Decimal.Parse(strNumber);
+                total += number;
+                startIndex = endIndex + 1;
+            }
+
+
+            return total;
+        }
+
+        public static decimal GrossPay(string strNumbers, decimal payRate)
+        {
+            decimal total = 0;
+            int startIndex = 0;
+            decimal grossPay = 0.0m;
+
+            while (startIndex < strNumbers.LastIndexOf(' '))
+            {
+                int endIndex = strNumbers.IndexOf(' ', startIndex);
+                string strNumber = strNumbers.Substring(startIndex, endIndex - startIndex);
+                Decimal number = Decimal.Parse(strNumber);
+                total += number;
+                startIndex = endIndex + 1;
+            }
+
+            if (total <= 40m)
+            {
+                grossPay = total * payRate;
+            }
+            else
+                grossPay = (40 * payRate) + (total - 40) * (payRate * 1.5m);
+
+            return grossPay;
+        }
+    }
+}
---
+++ tnation1730ex3b1/MainWindow.xaml.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace tnation1730ex3b1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CalcButton_Click(object sender, RoutedEventArgs e)
+        {
+            // 0) Calculate discountPercent
+            try
+            {
+                decimal subtotal = decimal.Parse(this.inputTextBox0a.Text);
+                decimal discountPercent = Ex3bCalculations.GetDiscountPercent(subtotal);
+                resultTextBox0.Text = discountPercent.ToString("f3");
+            }
+            catch {
+                resultTextBox0.Text = "";
+                MessageBox.Show("Invalid input: " + this.inputTextBox0a.Text);
+            }
+
+            // 1) Calculate discountPercent using output argument
+            try
+            {
+                decimal subtotal = decimal.Parse(this.inputTextBox1a.Text);
+                decimal discountPercent;
+                    Ex3bCalculations.GetDiscountPercent(subtotal, out discountPercent);
+                resultTextBox1.Text = discountPercent.ToString("f3");
+            }
+            catch
+            {
+                resultTextBox1.Text = "";
+                MessageBox.Show("Invalid input: " + this.inputTextBox1a.Text);
+            }
+
+            // 2) Calculate futureValue
+            try
+            {
+                int months = Int32.Parse(this.inputTextBox2a.Text);
+                decimal monthlyInvestment = Decimal.Parse(this.inputTextBox2b.Text);
+                decimal monthlyInterestRate = Decimal.Parse(this.inputTextBox2c.Text);
+                decimal futureValue = Ex3bCalculations.CalculateFutureValue(monthlyInvestment, monthlyInterestRate, months);
+                resultTextBox2.Text = futureValue.ToString("c2");
+            }
+            catch {
+                resultTextBox2.Text = "";
+                MessageBox.Show("Invalid input:\n" 
+                    + this.inputTextBox2a.Text + "\n"
+                    + this.inputTextBox2b.Text + "\n"
+                    + this.inputTextBox2c.Text + "\n");
+            }
+
+            // 3) Calculate futureValue using ref argument
+            try
+            {
+                int months = Int32.Parse(this.inputTextBox3a.Text);
+                decimal monthlyInvestment = Decimal.Parse(this.inputTextBox3b.Text);
+                decimal monthlyInterestRate = Decimal.Parse(this.inputTextBox3c.Text);
+                decimal futureValue = 0m;
+                Ex3bCalculations.CalculateFutureValue(monthlyInvestment, monthlyInterestRate, months, ref futureValue);
+                resultTextBox3.Text = futureValue.ToString("c2");
+            }
+            catch
+            {
+                resultTextBox3.Text = "";
+                MessageBox.Show("Invalid input:\n"
+                    + this.inputTextBox3a.Text + "\n"
+                    + this.inputTextBox3b.Text + "\n"
+                    + this.inputTextBox3c.Text + "\n");
+            }
+
+            // 4) Calculate degrees Celsius
+            try
+            {
+                double fahrenheit = double.Parse(this.inputTextBox4a.Text);
+                double celsius = Ex3bCalculations.FahrenheitToCelsius(fahrenheit);
+                resultTextBox4.Text = celsius.ToString("f1");
+            }
+            catch
+            {
+                resultTextBox4.Text = "";
+                MessageBox.Show("Invalid input: " + this.inputTextBox4a.Text);
+            }
+
+            // 5) Celsius degrees fahrenheit
+            try
+            {
+                double celsius = double.Parse(this.inputTextBox5a.Text);
+                double fahrenheit = 0.0;
+                    Ex3bCalculations.CelsiusToFahrenheit(celsius, out fahrenheit);
+                resultTextBox5.Text = fahrenheit.ToString("f1");
+            }
+            catch
+            {
+                resultTextBox5.Text = "";
+                MessageBox.Show("Invalid input: " + this.inputTextBox5a.Text);
+            }
+
+            // 6) Calculate GrossPay
+            try
+            {
+                decimal hours = decimal.Parse(this.inputTextBox6a.Text);
+                decimal payRate = decimal.Parse(this.inputTextBox6b.Text);
+                this.resultTextBox6.Text = Ex3bCalculations.GrossPay(hours, payRate).ToString("c2");
+            }
+            catch
+            {
+                resultTextBox6.Text = "";
+                MessageBox.Show("Invalid input:\n" 
+                    + this.inputTextBox6a.Text + "\n"
+                    + this.inputTextBox6b.Text);
+            }
+
+            // 7 TotalHours
+            try
+            {
+                this.resultTextBox7.Text =
+                    Ex3bCalculations.TotalHours(this.inputTextBox7a.Text).ToString("n2");
+            }
+            catch
+            {
+                this.resultTextBox7.Text = "";
+                MessageBox.Show("Invalid input: " + this.inputTextBox7a.Text);
+            }
+
+            // 8 GrossPay(strNumbers, rate)
+            try
+            {
+                decimal rate = Decimal.Parse(this.inputTextBox8b.Text);
+                this.resultTextBox8.Text =
+                    Ex3bCalculations.GrossPay(this.inputTextBox8a.Text, rate).ToString("c2");
+            }
+            catch
+            {
+                this.resultTextBox8.Text = "";
+                MessageBox.Show("Invalid input:\n" 
+                    + this.inputTextBox8a.Text + "\n"
+                    + this.inputTextBox8b.Text);
+            }
+        }
+    }
+}
